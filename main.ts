0fce2e779ae584e2e8d90f48292ec44c4d52dabd
L64k <76117921+L64k@users.noreply.github.com>
2023-07-31 08:41:45 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+let orientation = 0
+basic.forever(function () {
+    orientation = input.compassHeading()
+    if (orientation > 45 && orientation <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (orientation > 135 && orientation < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (orientation >= 225 && orientation < 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
